2cb314f8d3fa2b7806a234fe425d53ed54b14e82
007axe <xsumethix123@gmail.com>
2023-07-30 14:59:07 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/taiwind.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
